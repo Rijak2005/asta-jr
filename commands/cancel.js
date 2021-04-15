6033e2c2dadce6dffb67d63920f728d658b0cdfe
Rijak2005 <59411027+Rijak2005@users.noreply.github.com>
2021-04-15 14:28:08 +0530

    fixed cancel command

--- commands/cancel.js
+++ commands/cancel.js
@@ -12,7 +12,7 @@ module.exports = {
       return;
     }
     user.roles.add("814870385115136041");
-    user.roles.remove("814870385115136041");
+    user.roles.remove("814871546724220939");
     message.channel.send(
       `${user} who do you think you are? my master saw your metal bot ass cheating so I have removed you from premium and if you fool me again I will kick you out of this server understood!?`
     );
