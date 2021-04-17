f974aeb6572831afc268ffaad0e04c2918436f6d
Rijak2005 <59411027+Rijak2005@users.noreply.github.com>
2021-04-17 17:43:33 +0530

    updated whois command and added random

--- commands/who.js
+++ commands/who.js
@@ -1,11 +1,18 @@
+const random = require("random");
 module.exports = {
   name: "whois",
   decription: "whois command",
   execute(message, args) {
-    switch (args[0]) {
+    switch (args[0].toLowerCase()) {
       case "astatine":
         message.channel.send(
           "He is the greatest of all you nerds and he is my creator so yeah 😂 😁"
+        );
+        break;
+
+      case message.author.username.toLowerCase():
+        message.channel.send(
+          "Why u messin wid me bruh?! It's you, a person who has nothing to do except asking bots about themselves"
         );
         break;
 
@@ -24,9 +31,23 @@ module.exports = {
         break;
 
       default:
-        message.reply(
-          "I don't seem to recognize him  🤨 so he must be a stupid normie baby  👶🏻"
-        );
+        switch (random.int((min = 1), (max = 3))) {
+          case 2:
+            message.reply(
+              "Sounds like a great person... I actually kinda feel bad for not knowing them"
+            );
+            break;
+
+          case 3:
+            message.reply("A human 🤪");
+            break;
+
+          default:
+            message.reply(
+              "I don't seem to recognize him  🤨 so he must be a stupid normie baby  👶🏻"
+            );
+            break;
+        }
         break;
     }
   },
