af4205d22ba151a1f6042c150487707ee7d44d43
Rijak2005 <59411027+Rijak2005@users.noreply.github.com>
2021-09-16 16:06:04 +0530

    aded suggestion

--- commands/suggest.js
+++ commands/suggest.js
@@ -1,28 +1,50 @@
+var fs = require("fs");
+var path = require("path");
 module.exports = {
   name: "suggest",
   decription: "suggest command",
-  execute(message, args, bot) {
-    bot.channels.fetch("883253485657796608").then((ch) => {
-      ch.send({
+  async execute(message, args, bot) {
+    const likeEmoji = "👍🏽";
+    const dislikeEmoji = "👎🏽";
+    const pathToLog = path.join(
+      __dirname,
+      "..",
+      "models",
+      "suggestionData.txt"
+    );
+    var suggestions = fs.readFileSync(pathToLog, "utf8").split("\n");
+    var sentMessage = null;
+    // Chnage the channel in the main server
+    await bot.channels.fetch("883253485657796608").then(async (ch) => {
+      sentMessage = await ch.send({
         embed: {
-          title: `Suggestion from ${message.author.username}`,
+          // title: `💡 **Suggestion from ${message.author.tag}** 💡`,
+          title: `#${suggestions.length}: **${args.join(" ").toUpperCase()}**`,
           fields: [
+            // {
+            //   name: `${message.author.username} says:\n`,
+            //   value: `***${args.join(" ")}***`,
+            // },
             {
-              name: `${message.author.username} says:\n`,
-              value: `***${args.join(" ")}***`,
-            },
-            {
-              name: "​‎‎‎ ​‎‎‎ ​‎‎‎ ​‎‎‎ ​‎‎‎ ​‎‎‎ ​‎​‎‎‎",
-              value: "​‎‎‎ ​‎‎‎ ​‎‎‎ ​‎‎‎ ​‎‎‎ ​‎‎‎ ​‎​‎‎‎",
+              name: `💡 **A suggestion from ${message.author.tag}** 💡`,
+              value: `​‎‎‎ ​‎‎‎ ​‎‎‎ ​‎‎‎ ​‎‎‎ `,
             },
           ],
           footer: {
-            text: "To make your suggestion, please type: astajr suggest {your_suggestio_here} in any channel",
+            text: "To make your suggestion, please type: astajr suggest {your_suggestion_here} in any channel",
           },
           color: 0x39ff14,
         },
       });
     });
+    sentMessage.react(likeEmoji);
+    sentMessage.react(dislikeEmoji);
+    fs.writeFileSync(
+      pathToLog,
+      `${suggestions.join("\n")}\n#${suggestions.length}:${args.join(" ")} by ${
+        message.author.tag
+      } at ${new Date()}`
+    );
     message.reply("Suggestion posted!");
   },
 };
